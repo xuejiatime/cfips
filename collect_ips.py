464a8a7658a9114664a5a066d6ce631b844e5244
xuejiatime <19514521@qq.com>
2025-10-14 14:22:57 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -7,6 +7,7 @@ import os
 urls = [
     'https://ip.164746.xyz', 
     'https://cf.090227.xyz', 
+    'https://api.uouin.com/cloudflare.html',
     'https://stock.hostmonit.com/CloudFlareYes',
     'https://www.wetest.vip/page/cloudflare/address_v4.html'
 ]
